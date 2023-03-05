07f38e89ce71ec12af6faeb0b655bedf14806673
Tiago Gabriel <tiagogabrielcontato@hotmail.com>
2023-03-05 12:49:51 -0300

    05/03/2023 12:49 - Commit final, novo projeto em ts

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({content, onDeleteComment}){
+  const[likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment(){
     onDeleteComment(content);
+  }
+  function handleLikeComment(){
+    setLikeCount((state) => {
+      return likeCount + 1;
+    });
   }
   return(
     <div className={styles.comment}>
@@ -25,9 +33,9 @@ export function Comment({content, onDeleteComment}){
         <p>{content}</p>
       </div>
       <footer>
-        <button>
+        <button onClick={handleLikeComment}>
           <ThumbsUp/>
-          Aplaudir <span>20</span>
+          Aplaudir <span>{likeCount}</span>
         </button>
       </footer>
       </div>
